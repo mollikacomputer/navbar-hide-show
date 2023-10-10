5b9bb41bccf745e01630bc7518bc9c5f6adf63bb
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-10-10 08:08:32 +0600

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutPage = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-bold text-center text-blue-400' >About Page</h2>
+        </div>
+    );
+};
+
+export default aboutPage;
---
+++ src/app/blogs/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const blogPage = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold text-yellow-400 text-center">Blog page</h2>
+        </div>
+    );
+};
+
+export default blogPage;
